317c66def1fb92ce3ad2627c5465b0fff50f90e9
M. Mustafa Fındık <mustafafindik24@gmail.com>
2023-03-10 17:05:03 +0300

    clearCaching added and cacheCheck methods updated

--- src/main/java/com/mustafafindik/cacheapp/Controller/RedisCacheController.java
+++ src/main/java/com/mustafafindik/cacheapp/Controller/RedisCacheController.java
@@ -10,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisCacheController {
 
     private final RedisCacheService redisCacheService;
+    int counter = 0;
 
     public RedisCacheController(RedisCacheService redisCacheService) {
         this.redisCacheService = redisCacheService;
     }
     @GetMapping
     public String cacheCheck()throws Exception{
+        if (counter == 3){
+            redisCacheService.clearCaching();
+            counter=0;
+        }
+        counter++;
         return redisCacheService.runningMethod();
     }
 }
--- src/main/java/com/mustafafindik/cacheapp/Service/RedisCacheService.java
+++ src/main/java/com/mustafafindik/cacheapp/Service/RedisCacheService.java
@@ -1,14 +1,19 @@
 package com.mustafafindik.cacheapp.Service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisCacheService {
-
     @Cacheable(cacheNames = "myCacheMethod")
     public String runningMethod() throws Exception{
         Thread.sleep(5000L);
-        return "Metot çalıştı";
+        return "Metot çalıştırıldı.";
     }
+    @CacheEvict(cacheNames = "myCacheMethod")
+    public void clearCaching(){
+        System.out.println("Cache temizlendi.");
+    }
+
 }
